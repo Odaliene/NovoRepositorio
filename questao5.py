b8068b4e96129fffb8fa32f8761b2b6a745efd75
Odaliene <35998396+Odaliene@users.noreply.github.com>
2019-04-09 20:15:00 -0300

    Create questao5.py

---
+++ questao5.py
@@ -0,0 +1,12 @@
+soma = 0
+contaa = 0
+i = 0
+while (i<6):
+    n = float(input())
+    if n>0:
+        som+=n
+        cont+=1
+    i+=1
+        
+print("%d valores positivos" %cont)
+print("%.1f" %(soma/conta))
